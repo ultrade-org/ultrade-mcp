ea43024212f7d21dcf487f3153aad940bad2a5e5
MG <emg110@gmail.com>
2025-04-30 14:06:28 +0300

    Bump to 1.1.0

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,7 @@ class UltradeServer {
     this.server = new Server(
       {
         name: 'ultrade-mcp',
-        version: '1.0.0',
+        version: '1.1.0',
       },
       {
         capabilities: {
